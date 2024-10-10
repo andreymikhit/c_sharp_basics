2b53cf0c9b589a2608e2b0bab30a672eb7b920be
Andrey <mikhaylov.an.ham@gmail.com>
2024-10-11 01:02:36 +0200

    Create _HelloWorld_Program.cs

---
+++ les_3/_HelloWorld_Program.cs
@@ -0,0 +1,34 @@
+# Proj _HelloWorld_Program.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			//Console.WriteLine( "Я хочу изучать C#" );
+			//Console.ReadLine();
+
+			String a = Console.ReadLine();
+			String b = Console.ReadLine();
+
+			int num1 = Int32.Parse( a );
+			int num2 = Int32.Parse( b );
+
+			int result = num1 + num2;
+			Console.WriteLine( result );
+			result = num1 * num2;
+			Console.WriteLine( result );
+
+			double average = (double) ( num1 + num2 ) / 2;
+			Console.WriteLine( average );
+
+			Console.ReadLine();
+		}
+	}
+}
